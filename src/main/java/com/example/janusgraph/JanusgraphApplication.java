b25f7cbcd35d389e1b87fae5e6ab51b9f7c3fa83
libing <252356745@qq.com>
2019-08-30 20:43:02 +0800

    创建

---
+++ src/main/java/com/example/janusgraph/JanusgraphApplication.java
@@ -0,0 +1,13 @@
+package com.example.janusgraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JanusgraphApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JanusgraphApplication.class, args);
+    }
+
+}
